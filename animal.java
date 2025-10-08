8dadceef6ebf3d1ef08566f7db79c88836895311
GALINAGAKRISHNAREDDY <redy19822@gmail.com>
2025-10-08 07:32:35 +0530

    mm

---
+++ animal.java
@@ -0,0 +1,83 @@
+// class Animal
+// {
+//     void eat()
+//     {
+//     System.out.println("EATING...");
+//     }
+// }
+//     class Dog extends Animal
+//     {
+//         void bark()
+//         {
+//             System.out.println("barking...");
+
+//         }
+//     }
+//         class Testlnheritance{
+//             public static void main (String args [])
+//             {
+//                 Dog d=new Dog();
+//                 d.bark();
+//                 d.eat();
+//             }
+//         }
+    
+// class jenthuvu
+// {
+// void eat()
+// {
+// System.out.println("eating");
+// }
+// }
+// class Kukka extends jenthuvu
+// {
+// void ishowspeed()
+// {
+// System.out.println("ishowspeeding");
+// }
+// }
+// class TestInheritance{
+// public static void main(String args[])
+// {
+// Kukka k= new Kukka();
+// k.ishowspeed();
+// k.eat();
+// }}
+
+    
+class Animal
+{
+    void eat()
+    {
+        System.out.println(eating);
+
+    }
+}
+class Dog extends Animal{
+    void bark()
+    {
+        System.out.println(barking);
+
+
+    }
+}
+class panda extends Animal
+{
+    void sleep()
+    {
+        System.out.println(sleepiing);
+
+    }
+}
+class TestInheritance
+{
+    public static void main (String args [])
+}
+{
+    dog d= new Dog();
+    d.bark();
+    d.eat();
+    panda p=new panda();
+    p.sleep();
+    
+}
